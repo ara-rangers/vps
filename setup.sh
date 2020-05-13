f2fa3b76662490f8e8d3ac66b3b6118664d16ab1
ara-rangers <65111409+ara-rangers@users.noreply.github.com>
2020-05-13 21:16:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -271,7 +271,7 @@ service ssh restart
 service openvpn restart
 service dropbear restart
 service nginx restart
-service php5-fpm restart
+service php7.0-fpm restart
 service webmin restart
 service squid3 restart
 service fail2ban restart
